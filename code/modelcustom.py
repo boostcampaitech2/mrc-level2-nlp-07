2819e162035fb876dd442dda522c08a2fb920faa
bowsong <hookypooky@naver.com>
2021-10-31 08:24:04 +0000

    model done, thanks for the help:)

--- code/modelcustom.py
+++ code/modelcustom.py
@@ -1,9 +1,10 @@
 from transformers import AutoModel
 import torch
 import torch.nn as nn
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 import numpy as np
 import torch.nn.functional as F
-
+from transformers.modeling_outputs import QuestionAnsweringModelOutput
 device = "cuda:0"
 class QAWithLSTMModel(nn.Module):
     def __init__(self,model_args,config):
@@ -11,15 +12,15 @@ class QAWithLSTMModel(nn.Module):
         self.model_name = model_args
         self.pretrained = AutoModel.from_pretrained(self.model_name,config=config)
         self.LSTM1 = nn.LSTM(input_size=config.hidden_size,hidden_size=config.hidden_size, batch_first=True,bidirectional=True)
-        self.maxpool = nn.MaxPool1d(2,stride=2)
+        self.maxpool = nn.MaxPool1d(2,stride=3)
         self.gelu = F.gelu
         self.flatflat = nn.Flatten()
-        self.classify = nn.Linear(384*1536,768,bias=True)
+        self.classify = nn.Linear(config.hidden_size,2,bias=True)
         self.softmax = nn.Softmax()
 
     def forward(self, input_ids=None, attention_mask=None, token_type_ids=None, position_ids=None, head_mask=None, inputs_embeds=None, start_positions=None, end_positions=None, output_attentions=None, output_hidden_states=None, return_dict=None):
         
-        #with torch.no_grad():
+        #with torch.no_grad():   #얼리거나 얼리지 않거나
         back_output = self.pretrained(input_ids,
                 attention_mask=attention_mask,
                 token_type_ids=token_type_ids,
@@ -28,26 +29,54 @@ class QAWithLSTMModel(nn.Module):
                 output_attentions=output_attentions,
                 output_hidden_states=output_hidden_states,
                 return_dict=return_dict)
-        back_output = torch.tensor(back_output[0]).cuda()
+        back_output = back_output[0]
         back_output_pooled = back_output
+        
 
         lstm_output,(hidden_h,hidden_c) = self.LSTM1(back_output_pooled)
         concat_vector = torch.cat((lstm_output,back_output_pooled),-1)
-    
         embeded_vector = self.maxpool(concat_vector)
-        print("size after maxpool:" ,embeded_vector.size())
         logit = self.gelu(embeded_vector)
-        logit = self.flatflat(logit)
-        print("size after flatten : " ,logit.size())
         logit = self.classify(logit)
         logit = self.softmax(logit)*1000
-        start_logits,end_logits = logit.split(384,dim=-1)
+        start_logits,end_logits = logit.split(1,dim=-1)
+        start_logits = start_logits.squeeze(-1).contiguous()
+        end_logits = end_logits.squeeze(-1).contiguous()
+
+
+
+        total_loss = None
+        if start_positions is not None and end_positions is not None:
+            # If we are on multi-GPU, split add a dimension
+            if len(start_positions.size()) > 1:
+                start_positions = start_positions.squeeze(-1)
+            if len(end_positions.size()) > 1:
+                end_positions = end_positions.squeeze(-1)
+            # sometimes the start/end positions are outside our model inputs, we ignore these terms
+            ignored_index = start_logits.size(1)
+            start_positions = start_positions.clamp(0, ignored_index)
+            end_positions = end_positions.clamp(0, ignored_index)
+
+            loss_fct = CrossEntropyLoss(ignore_index=ignored_index)
+            start_loss = loss_fct(start_logits, start_positions)
+            end_loss = loss_fct(end_logits, end_positions)
+            total_loss = (start_loss + end_loss) / 2
+            
+
+        if not return_dict:
+            output = (start_logits, end_logits) 
+            return ((total_loss,) + output) if total_loss is not None else output
+
+        return QuestionAnsweringModelOutput(
+            loss=total_loss,
+            start_logits=start_logits,
+            end_logits=end_logits,
+            hidden_states=output_hidden_states,
+            attentions=output_attentions,
+        )
         
-        # start_logits = start_logits.squeeze(-1)
-        # end_logits = end_logits.squeeze(-1)
-        
-        return  {"start_logits" : start_logits, "end_logits" : end_logits, "hidden_states" :  None, "attentions" : None}
-        
+
+
     def make_token_type_ids(self, input_ids) :
         token_type_ids = []
         for i, input_id in enumerate(input_ids):
